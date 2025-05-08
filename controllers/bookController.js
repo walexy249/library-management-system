f85eda3a8323e75a08d79d81b6782f232aff2e7a
Lawal Olawale <lawalolawale32@gmail.com>
2025-05-08 23:27:41 +0100

    add controllers ro borrow and return books

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,10 +1,22 @@
-const Book = require("../models/bookModel");
+const Book = require('../models/bookModel');
 
 exports.getAllBooks = async (req, res, next) => {
   const books = await Book.find();
 
   res.status(200).json({
-    status: "success",
+    status: 'success',
+    results: books.length,
+    data: {
+      books,
+    },
+  });
+};
+
+exports.getAvailableBooks = async (req, res, next) => {
+  const books = await Book.find({ isBorrowed: false });
+
+  res.status(200).json({
+    status: 'success',
     results: books.length,
     data: {
       books,
@@ -21,14 +33,96 @@ exports.createBook = async (req, res, next) => {
     });
 
     res.status(200).json({
-      status: "success",
+      status: 'success',
       data: {
         book,
       },
     });
   } catch (err) {
     res.status(400).json({
-      status: "fail",
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.borrowBook = async (req, res, next) => {
+  try {
+    console.log(req.params.id);
+    const book = await Book.findById(req.params.id);
+
+    if (!book)
+      return res.status(400).json({
+        status: 'fail',
+        message: 'Book not found.There is no book with that ID',
+      });
+
+    if (book.isBorrowed)
+      return res.status(400).json({
+        status: 'fail',
+        message: 'Book is borrowed already',
+      });
+
+    if (!req.body.borrower)
+      return res.status(400).json({
+        status: 'fail',
+        message: 'Borrower is required',
+      });
+
+    book.isBorrowed = true;
+    book.borrower = req.body.borrower;
+    book.borrowedAt = new Date();
+
+    await book.save();
+
+    console.log('saving-----');
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        book,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.returnBook = async (req, res, next) => {
+  try {
+    const book = await Book.findById(req.params.id);
+
+    if (!book)
+      return res.status(400).json({
+        status: 'fail',
+        message: 'Book not found.There is no book with that ID',
+      });
+
+    if (!book.isBorrowed)
+      return res.status(400).json({
+        status: 'fail',
+        message:
+          'Book is not borrowed. You can only return borrowed borrowed books',
+      });
+
+    book.isBorrowed = false;
+    book.borrower = null;
+    book.borrowedAt = null;
+
+    await book.save();
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        book,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
       message: err,
     });
   }
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,11 +1,15 @@
-const express = require("express");
-const bookController = require("../controllers/bookController");
+const express = require('express');
+const bookController = require('../controllers/bookController');
 
 const router = express.Router();
 
 router
-  .route("/")
+  .route('/')
   .get(bookController.getAllBooks)
   .post(bookController.createBook);
+
+router.route('/available').get(bookController.getAvailableBooks);
+router.route('/:id/borrow').post(bookController.borrowBook);
+router.route('/:id/return').post(bookController.returnBook);
 
 module.exports = router;
