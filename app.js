ef3c11c2182222837e481e72afa2097a827b7340
Lawal Olawale <lawalolawale32@gmail.com>
2025-05-08 16:30:03 +0100

    setup express and mongoose

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
+dotenv.config({ path: "./config.env" });
 const app = express();
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log("App running on port 3000");
+});
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => {
+    console.log("Database connection successful ✅");
+  })
+  .catch((err) => {
+    console.log("Error", err);
+  });
